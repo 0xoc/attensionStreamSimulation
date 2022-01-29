30a892529ec223fb0018de07b1281d8b18c37ac7
Ali Parvizi <snparvizi75@gmail.com>
2022-01-29 03:39:57 -0800

    refine models

--- core/models.py
+++ core/models.py
@@ -11,26 +11,24 @@ class TopicRule(models.Model):
     topic = models.ForeignKey("Topic", related_name="rules", on_delete=models.CASCADE)
 
 
-class Token(models.Model):
-    name = models.CharField(max_length=255)
-    symbol = models.CharField(max_length=255)
-
-
 class Tag(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, blank=True, null=True)
+    label = models.CharField(max_length=255)
 
 
 class Arena(models.Model):
     description = models.TextField()
-    token = models.ForeignKey(Token, related_name="arenas", on_delete=models.CASCADE)
+    token_contract_address = models.CharField(max_length=255)
+
     min_contribution_amount = models.IntegerField()
 
     fund_address = models.CharField(max_length=255)
-    percent_fee = models.FloatField(default=0)
+    arena_fund_fee_percentage = models.FloatField(default=0)
 
     allow_topic_funds = models.BooleanField(default=False)
     allow_choice_funds = models.BooleanField(default=False)
+
+    topic_creation_fee = models.FloatField()
+    choice_creation_fee = models.FloatField()
 
     tags = models.ManyToManyField(Tag)
 
@@ -38,21 +36,23 @@ class Arena(models.Model):
 class Topic(models.Model):
     description = models.TextField()
     arena = models.ForeignKey(Arena, related_name="topics", on_delete=models.CASCADE)
+
+    create_datetime = models.DateTimeField()
     cycle_duration = models.DurationField()
 
-    topic_duration_in_cycles = models.IntegerField()
-
-    share_per_cycle_percentage = models.FloatField(default=0)
+    transfer_period = models.DurationField()
 
     choice_min_support_threshold = models.FloatField(default=0)
-    choice_min_support_duration = models.DurationField()
 
+    share_per_cycle_percentage = models.FloatField(default=0)
     prev_contributors_fee_percentage = models.FloatField(default=0)
 
     topic_fund_address = models.CharField(max_length=255, blank=True, null=True)
     topic_fund_fee_percentage = models.FloatField(default=0)
 
     allow_choice_funds = models.BooleanField(default=False)
+
+    max_target_amount = models.FloatField()
 
     tags = models.ManyToManyField(Tag)
     # todo: how should the end of a topic be described? for example in terms of how many cycles have passed?
@@ -62,9 +62,7 @@ class Choice(models.Model):
     topic = models.ForeignKey(Topic, related_name="choices", on_delete=models.CASCADE)
     description = models.TextField()
 
-    conflicts_with = models.ManyToManyField("Choice")
-
     choice_fund_address = models.CharField(max_length=255)
     fee_percentage = models.FloatField(default=0)
 
-    target_amount = models.IntegerField(default=10000000000)
+    target_amount = models.FloatField()
